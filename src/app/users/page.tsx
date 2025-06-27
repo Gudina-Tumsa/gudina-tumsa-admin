8da0d98cb38ddfd6af05c5b0dd4f3db890d92662
abenezer121 <webgoat12@gmail.com>
2025-06-27 16:35:00 +0300

    [feat]- fixing table for books

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
+import { DataTable } from "./data-table"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import {
@@ -8,7 +8,7 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
-import data from "../data.json"
+import data from "./data.json"
 
 export default function Page() {
   return (
