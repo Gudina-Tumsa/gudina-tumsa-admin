06b715b9fbcf96cef2a3f4caa1917b881ca98153
abenezer121 <webgoat12@gmail.com>
2026-01-03 12:24:55 +0300

    fixes

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
     env: {
 
       //  NEXT_PUBLIC_BASE_URL:  "https://api.gudinatumsa.com",
-       NEXT_PUBLIC_BASE_URL:  "http://localhost:3001",
-        //NEXT_PUBLIC_BASE_URL: "https://api.gudinatumsa.com"
+       //NEXT_PUBLIC_BASE_URL:  "http://localhost:3001",
+        NEXT_PUBLIC_BASE_URL: "https://api.gudinatumsa.com"
 
     },
 };
